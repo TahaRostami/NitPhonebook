60a421a5b11c996178ad3fb9d9387be7ecf754cb
tataiee1375 <tataiee1375@gmail.com>
2018-05-22 00:25:12 +0430

    NitPhonebook

---
+++ Nit.Phonebook/Nit.Phonebook/Models/Data/ChangeTracingkInformation.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nit.Phonebook.Models.Data
+{
+    [Table("ChangeTracingkInformation")]
+    public partial class ChangeTracingkInformation
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string TableName { get; set; }
+
+        [Required]
+        public string Action { get; set; }
+
+
+        public string LastTime { get; set; }
+
+        public string LastId { get; set; }
+
+    }
+}
